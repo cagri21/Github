f7ebb00ca83d8e89bf882704691ec0d1674476b3
Çağrı Yörükoğlu <cagriyorukoglu21@gmail.com>
2026-04-21 23:52:43 +0300

    Initial Commit

---
+++ Github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github
+//
+//  Created by Yorukoglu, Cagri on 21.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Yorukoglu, Cagri on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
